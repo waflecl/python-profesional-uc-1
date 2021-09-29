7c508c00e68aca24f7af27ca175eb65e8c0b8cf2
dhenriquez <contacto@dhenriquez.cl>
2021-09-29 15:51:39 -0300

    correccion

--- Python y Bases de Datos 20214_DCC_PBD100N/Modulo 2/MiniProyecto1/MiniProyecto1.py
+++ Python y Bases de Datos 20214_DCC_PBD100N/Modulo 2/MiniProyecto1/MiniProyecto1.py
@@ -13,11 +13,17 @@ mydb = db.connect(
 cur = mydb.cursor()
 
 # c) Agregar datos a la tabla
-cur.execute("INSERT INTO Aeropuertos(id, ident, type, name, elevation_ft, municipality, iata_code, score)VALUES('39340','SHCC','heliport','Clínica Las Condes Heliport','2461','Santiago','','25');")
-cur.execute("INSERT INTO Aeropuertos(id, ident, type, name, elevation_ft, municipality, iata_code, score)VALUES('39379','SHMA','heliport','Clínica Santa María Heliport','2028','Santiago','','25');")
-cur.execute("INSERT INTO Aeropuertos(id, ident, type, name, elevation_ft, municipality, iata_code, score)VALUES('39390','SHPT','heliport','Portillo Heliport','9000','Los Andes','','25');")
+
+query = 'INSERT INTO Aeropuertos(id, ident, type, name, elevation_ft, municipality, iata_code, score)VALUES(%s,%s,%s,%s,%s,%s,%s,%s)'
+data = [
+    ('39340','SHCC','heliport','Clínica Las Condes Heliport', 2461,'Santiago','', 25),
+    ('39379','SHMA','heliport','Clínica Santa María Heliport', 2028,'Santiago','', 25),
+    ('39390','SHPT','heliport','Portillo Heliport', 9000,'Los Andes','',25)
+]
+cur.executemany(query, data)
 mydb.commit()
 
 cur.execute("SELECT name as Nombre, type as Tipo, municipality as Municipalidad, elevation_ft as Altura FROM InfoAeropuertos.Aeropuertos WHERE elevation_ft > 5000")
-for d in cur:
+rows = cur.fetchall()
+for d in rows:
     print(d)
