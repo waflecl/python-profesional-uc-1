ff8039f27c2d750e2fe47b253c9b7ed132561397
dhenriquez <contacto@dhenriquez.cl>
2021-12-14 21:31:29 -0300

    MP1

--- Desarrollo de Software con Python 20215_DCC_SOF096N/Modulo 2/Miniproyecto 1/main.py
+++ Desarrollo de Software con Python 20215_DCC_SOF096N/Modulo 2/Miniproyecto 1/main.py
@@ -48,9 +48,7 @@ def cargar_datos(archivo, partidas, datos_partidas):
             ##    Partidas debe ser un conjunto con pares id_partida y nombre_partida. 
             ## COMPLETAR AQUí
 
-            ##
-            ##
-            ##
+            partidas.add((int(id_partida), nombre_partida))
 
             ## 1. Agregar datos a estructuras apropiadas.
             ##    Construir diccionario con informacion de todas las ejecuciones de una partida
@@ -58,9 +56,8 @@ def cargar_datos(archivo, partidas, datos_partidas):
             ##    de todos los subtítulos de esa partida.
             ## COMPLETAR AQUI
 
-            ##
-            ##
-            ##
+            datos_partidas.setdefault(int(id_partida), [])
+            datos_partidas[int(id_partida)].append({'id_subtitulo': id_subtitulo, 'nombre_subtitulo': nombre_subtitulo, 'monto_original': monto_original, 'monto_a_marzo': monto_a_marzo, 'moneda': moneda})
 
     print("Datos leidos.")
     print("Cantidad de partidas distintas leidas:", len(partidas))
@@ -75,9 +72,27 @@ def cargar_datos(archivo, partidas, datos_partidas):
 def montos_por_partida(partidas, datos_partidas, usd_a_clp):
     pass
     ## COMPLETAR AQUI
-    ##
-    ##
-    ##
+    op = -1
+    while op != 0:
+        for partida in partidas:
+            print(str(partida[0]) + ": " + partida[1])
+        print("0: Menú Principal")
+        try:
+            op = int(input("Opcion: "))
+        
+        except ValueError:
+            print(f"Ingrese opción valida")
+            op = -1
+        
+        if op != 0 and op in datos_partidas:
+          print("Nombre Subtitulo | Monto Original | Monto Marzo 2020 | Diferencia")
+          for v in datos_partidas[op]:
+            if v['moneda'] == 'USD':
+              v['monto_original'] = v['monto_original'] * usd_a_clp
+              v['monto_a_marzo'] = v['monto_a_marzo'] * usd_a_clp
+            diferencia = v['monto_original'] - v['monto_a_marzo']
+            print(f"{v['nombre_subtitulo']} | {v['monto_original']} | {v['monto_a_marzo']} | {diferencia}")
+          op = 0
 
 
 
@@ -87,9 +102,18 @@ def montos_por_partida(partidas, datos_partidas, usd_a_clp):
 def partidas_por_monto_original(partidas, datos_partidas, usd_a_clp):
     pass
     ## COMPLETAR AQUI
-    ##
-    ##
-    ##
+    partida_monto_total = {}
+    for dato_partida in datos_partidas.items():
+      monto_total = 0
+      id_partida = dato_partida[0]
+      for subtitulo in dato_partida[1]:
+        if subtitulo['moneda'] == 'USD':
+          subtitulo['monto_original'] = subtitulo['monto_original'] * usd_a_clp
+        monto_total += subtitulo['monto_original']
+      partida_monto_total[id_partida] = monto_total
+    partida_monto_total_desc = dict(sorted(partida_monto_total.items(), key = lambda x: x[1], reverse = True))
+    for k, v in partida_monto_total_desc.items():
+      print("Partida ID: " + str(k) + " Monto Total: " + str(v))
 
 
 
@@ -100,11 +124,48 @@ def partidas_por_monto_original(partidas, datos_partidas, usd_a_clp):
 def funcion_personalizada(partidas, datos_partidas, usd_a_clp):
     pass
     ## COMPLETAR AQUI
-    ##
-    ##
-    ##
+    ## Consulta similar a la primera pero el contenido se carga en un DataFrame de Pandas
+    ## y permite guardar la información como Excel o mostrar en pantalla
+    import pandas as pd
+    rows = []
+    op = -1
+    while op != 0:
+        for partida in partidas:
+            print(str(partida[0]) + ": " + partida[1])
+        print("0: Volver al Menú Principal")
+        try:
+            op = int(input("Opción: "))
+        
+        except ValueError:
+            print(f"Ingrese opción válida")
+            op = -1
 
-
+        if op != 0 and op in datos_partidas:
+          for v in datos_partidas[op]:
+            if v['moneda'] == 'USD':
+              v['monto_original'] = v['monto_original'] * usd_a_clp
+              v['monto_a_marzo'] = v['monto_a_marzo'] * usd_a_clp
+            diferencia = v['monto_original'] - v['monto_a_marzo']
+            rows.append([v['nombre_subtitulo'], v['monto_original'], v['monto_a_marzo'], diferencia])
+          op = 0
+        df = pd.DataFrame(rows)
+        df.columns = ["Nombre Subtitulo","Monto Original","Monto Marzo 2020","Diferencia"]
+        opg = -1
+        while opg != 0:
+          print("1: Mostrar Pandas DataFrame")
+          print("2: Guardar Pandas DataFrame como Excel")
+          print("0: Volver al Menú Principal")
+          try:
+            opg = int(input("Opción: "))
+          except ValueError:
+            print("Ingrese opción válida")
+            opg = -1
+          if opg != 0:
+            if opg == 1:
+              print(df)
+            elif opg == 2:
+              df.to_excel("subtitulo.xlsx")
+              print("Archivo Guardado")
 
 ############################################################################
 ## No necesita modificar este código ##
